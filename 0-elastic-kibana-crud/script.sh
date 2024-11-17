22f3877e2b7fa14f7e5939fc77ade0e8b3d56a24
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-11-17 14:44:27 +0100

    chore: update script for necessary data

--- 0-elastic-kibana-crud/script.sh
+++ 0-elastic-kibana-crud/script.sh
@@ -1,7 +1,4 @@
-USER=elastic
-PASSWORD=$(echo y | docker exec -i elasticsearch /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic 2> /dev/null | grep "New value:" | awk '{print $3}')
-KIBANA_TOKEN=$(docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana 2> /dev/null)
-echo "USER=$USER" > ./credentials/credentials.txt
-echo "PASSWORD=$PASSWORD" >> ./credentials/credentials.txt
-echo "KIBANA_TOKEN=$KIBANA_TOKEN" >> ./credentials/credentials.txt
-docker cp elasticsearch:/usr/share/elasticsearch/config/certs/http_ca.crt ./credentials
+KIBANA_TOKEN=$(docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana 2> /dev/null | grep -v WARNING)
+KIBANA_VERIFICATION_CODE=$(docker exec kibana bin/kibana-verification-code | grep "verification code is:" | awk '{print $5 $6}' 2> /dev/null)
+echo "KIBANA_TOKEN=$KIBANA_TOKEN" > ./credentials/credentials.txt
+echo "KIBANA_VERIFICATION_CODE=$KIBANA_VERIFICATION_CODE" >> ./credentials/credentials.txt
