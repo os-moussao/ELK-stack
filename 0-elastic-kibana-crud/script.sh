8635139be5fc6a90d6f24cbc55bdb21e4ccf809d
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-11-20 12:48:59 +0100

    chore: remove non working setup

--- 0-elastic-kibana-crud/script.sh
+++
@@ -1,4 +0,0 @@
-KIBANA_TOKEN=$(docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana 2> /dev/null | grep -v WARNING)
-KIBANA_VERIFICATION_CODE=$(docker exec kibana bin/kibana-verification-code | grep "verification code is:" | awk '{print $5 $6}' 2> /dev/null)
-echo "KIBANA_TOKEN=$KIBANA_TOKEN" > ./credentials/credentials.txt
-echo "KIBANA_VERIFICATION_CODE=$KIBANA_VERIFICATION_CODE" >> ./credentials/credentials.txt
